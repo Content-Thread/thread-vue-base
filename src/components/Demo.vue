54fdbaac0be128060dbb39e50ad92e00c6be8315
Nate Steiner <nsteiner@gmail.com>
2019-03-07 10:03:20 -0500

    reconfig for library style reuse

---
+++ src/components/Demo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>Demo of Components</h2>
+    <Banner>This is a banner</Banner>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Demo",
+  data: () => ({}),
+  methods: {}
+};
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
-import HelloWorld from './components/HelloWorld.vue'
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from "vue";
+import App from "./App";
+import "./components";
 
-export default HelloWorld
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  render: h => h(App)
+}).$mount("#app");
