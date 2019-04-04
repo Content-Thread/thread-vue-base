9951e4a07918cd7e788754c23d5c7c5cb2567223
Mark Noonan <markthomasnoonan@gmail.com>
2019-04-04 13:39:04 -0400

    revert base button behavior

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,5 +1,9 @@
 <template>
-  <button v-on="listeners" v-bind="$attrs">{{labelText}}</button>
+
+<button>
+  {{labelText}}
+</button>
+
 </template>
 
 <script>
@@ -13,36 +17,15 @@ export default {
     labelText: {
       type: String,
       default: () => "no label provided"
-    },
-    apiUrl: {
-      type: String,
-      default: () => ""
-    },
-    payload: {
-      type: Object,
-      default: () => {}
-    }
-  },
-  methods: {
-    checkSubmit(event) {
-      if (event.target.type === "submit" && this.apiUrl.length) {
-        console.log('Will submit to ' + this.apiUrl + "with this data", JSON.stringify(this.payload));
-        this.axios.post(this.apiUrl, this.payload).then((response) => {
-          console.log(response.data)
-      })
-      } else {
-        console.log("will not submit, type is: " + event.target.type + ' and API URL has length ' + this.apiUrl.length);
-      }
-
     }
   },
   computed: {
     listeners() {
       return {
         ...this.$listeners,
-        input: event => this.$emit("input", event.target.value),
-        click: this.checkSubmit
-      };
+        input: event => 
+          this.$emit('input', event.target.value)
+      }
     }
   }
 };
