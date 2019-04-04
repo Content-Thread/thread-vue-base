3b28d7ae1d367387adb59ecf01a9a0d5b378b669
Mark Noonan <markthomasnoonan@gmail.com>
2019-04-04 14:31:40 -0400

    update demo to use base form

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -3,35 +3,32 @@
     <h1>Demo of Components</h1>
     <Banner>This is a banner</Banner>
     <hr>
-    <h2>Base Text Input</h2>
-    <BaseTextInput v-model="textval" @input="logInput" :class="{
-      'has-error': $v.textval.$invalid
-    }"/>
-    <hr>
-    <h2>Base Checkbox Input</h2>
-    <BaseCheckbox @input="logInput"/>
-    <hr>
-    <h2>Base Email Input</h2>
-    <BaseEmailInput @input="logInput"/>
-    <hr>
-    <h2>Base Phone Input</h2>
-    <BasePhoneInput @input="logInput"/>
-    <hr>
-    <h2>Base Number Input</h2>
-    <BaseNumberInput @input="logInput"/>
-    <hr>
-    <h2>Base Select Input</h2>
-    <BaseSelect @input="logInput">
-    </BaseSelect>
-    <hr>
-    <h2>Buttons</h2>
-    <p>Check console to see button output.</p>
-    <h3>Base Button (no type provided, defaults to "submit", apiUrl prop is set)</h3>
-    <BaseButton apiUrl="https://swapi.co/api/people/2" :payload="validations"/>
-    <h3>Base Button (type="button")</h3>
-    <BaseButton type="button"/>
-    <h3>Base Button (type="submit")</h3>
-    <BaseButton type="submit"/>
+    <h2>Base Form</h2>
+    <BaseForm apiUrl="https://swapi.co/api/people/2" :payload="validations">
+      <h2>Base Text Input</h2>
+      <BaseTextInput v-model="textval" @input="logInput" :class="{
+        'has-error': $v.textval.$invalid
+      }"/>
+      <hr>
+      <h2>Base Checkbox Input</h2>
+      <BaseCheckbox @input="logInput"/>
+      <hr>
+      <h2>Base Email Input</h2>
+      <BaseEmailInput @input="logInput"/>
+      <hr>
+      <h2>Base Phone Input</h2>
+      <BasePhoneInput @input="logInput"/>
+      <hr>
+      <h2>Base Number Input</h2>
+      <BaseNumberInput @input="logInput"/>
+      <hr>
+      <h2>Base Select Input</h2>
+      <BaseSelect @input="logInput">
+      </BaseSelect>
+      <hr>
+      <h3>Base Button</h3>
+      <BaseButton/>
+    </BaseForm>
   </div>
 </template>
 
